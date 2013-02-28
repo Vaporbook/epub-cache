ff6e65ed3bd0be2e3dfb5b5a644347e86fc73cb0
Vaporbook <aaron@readsocialapi.com>
2013-02-28 10:25:06 -0500

    removed automatic cacheId generation

--- test/test.js
+++ test/test.js
@@ -4,44 +4,62 @@ if(!process.argv[2]) {
   throw "You must supply a path to a valid EPUB file!";
 }
 
-// 1. init the cache - older sub-folders that exceed limit will be purged
+// 1. init the cache
 
 cacher.init({
 
-	cacheDir: '/Users/asm/testcache',
+	cacheDir: './test-cache',
 	idLimit: 100
 
 });
 
 console.log('clearing cache...');
 
-cacher.clear();
+try {
+	cacher.clear();
+	console.log('SUCCESS: cleared');
+} catch(e) {
+	console.log('ERROR clearing cache');
+}
 
 // 2. cache epub contents and get a cache id back
 
-var epubfile = process.argv[2];
+var epubfile = process.argv[2] || './testbook.epub';
 
 if(typeof epubfile === 'undefined') throw "You must provide an epub filename.";
+var cacheId = (new Date()).getTime();
+cacher.cache(epubfile, cacheId, function (err, cacheId, bundle) {
 
-cacher.cache(epubfile, function (cacheId) {
+	if(err) throw err;
 
 	console.log('cached epub file '+epubfile+'. cacheId is '+cacheId);
 
 	// 3. see if the cacheId exists
 
+	console.log('testing the cacher.has() method... should find in cache');
+
 	if(cacher.has(cacheId)) {
 
-		console.log('found cached id '+cacheId+' in cache');
+		console.log('SUCCESS: found cached id '+cacheId+' in cache');
 
 	}
 
-	// 4. cache it as a different id this time
-	cacher.cache(epubfile, 'foobar7', function (cacheId) {
 
-		console.log('cached epub file '+epubfile+'. cacheId is '+cacheId);
+});
 
+console.log('testing to see if we pull from cache the second time...');
 
-	});
+cacher.cache(epubfile, cacheId, function (err, cacheId, bundle) {
+
+	if(err) throw err;
+
+	console.log('cached epub file '+epubfile+'. cacheId is '+cacheId);
 
 
 });
+
+
+
+
+
+
